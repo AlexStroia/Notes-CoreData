7df4ab1de74f3a0734e2565f8a1ddb83c1b09742
Stroia Alexandru <stroia.alexandru@assist.ro>
2018-06-05 11:59:06 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Alex Stroia on 6/5/18.
+//  Copyright © 2018 Alex Stroia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
